37b02295de68199a7526cba367b0c81edcc4d71d
James <jamesarnott2@gmail.com>
2022-07-29 02:15:49 +0100

    fix: error with libraries

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      './runtimeConfig': './runtimeConfig.browser',
     },
   },
   server: {
